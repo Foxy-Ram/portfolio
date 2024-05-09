ab628f0e4510c63381fea0c3f3ac529ecb5bf106
Foxy-Ram <129158822+Foxy-Ram@users.noreply.github.com>
2024-05-09 13:38:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,25 @@ function continuousTypeWriter(text, speed) {
 
   // Call the continuousTypeWriter function
   continuousTypeWriter(titleText, 200);
+
+
+
+//Custom Cursor
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove",function(e){
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  /* cursorOutline.style.left = `${posX}px`;
+  cursorOutline.style.top = `${posY}px`; */
+
+  cursorOutline.animate({ left:`${posX}px`, top: `${posY}px`},
+  
+                        {duration:1000,fill:"forwards"});
+
+})
